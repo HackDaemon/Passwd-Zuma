90b0afcf80dd1ee22bcfd1d82ad7867b51dc11f3
AbyChan <crystal.tyan@gmail.com>
2015-08-22 01:01:58 +0800

    thinking

--- passzm/core/zuma.py
+++ passzm/core/zuma.py
@@ -6,3 +6,13 @@ import core.transformer
 def spiral(rawtxt):
 	#config, material = sorter.pickup(rawtxt)
 	sorter.pickup(rawtxt)
+
+
+class Zuma:
+
+	def __init__(self, rawtxt):
+		self.config, self.classify = sorter.pickup(tawtxt)
+
+	def register(self, tactice):
+		pass
+
